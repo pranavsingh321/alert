5e8fefeba32995b39acb1b22e334179e16e1375f
Pranav Singh <pranav.singh@tsystems.com>
2025-04-02 08:25:27 +0000

    initial

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,1 @@
-document.addEventListener("htmx:afterProcess", function (event) {
-  if (event.detail.xhr.responseURL.includes("/chart-data")) {
-    const chartData = JSON.parse(event.detail.xhr.responseText);
-    const layout = {
-      title: "Interactive Time Series Chart",
-      xaxis: { title: "Date" },
-      yaxis: { title: "Value" },
-    };
 
-    // Ensure the chart container exists before rendering
-    if (document.getElementById("chart-container")) {
-      Plotly.newPlot("chart-container", [chartData], layout);
-    } else {
-      console.error("Chart container not found.");
-    }
-  }
-});
-
-console.log("after process");
